9c6814d66f52bdbb92304d3c8d49cd9c24318bca
Student 12 <student_12@aiu.local>
2024-12-13 09:10:05 +0500

    Поправил код

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 int main()
 {
- int a, b;
- cout << "Enter A and B: ";
- cin >> a >> b;
- cout << "A + B = " << a + b << '\n'
-      << "A - B = " << a - b << '\n'
-      << "A * B = " << a * b << '\n'
-      << "A : B = " << a / b << '\n';
- return 0;
+    int a, b;
+    std::cout << "Enter A and B: ";
+    std::cin >> a >> b;
+    std::cout << "A + B = " << a + b << '\n'
+        << "A - B = " << a - b << '\n'
+        << "A * B = " << a * b << '\n'
+        << "A : B = " << a / b << '\n';
+    return 0;
 }
